358200dc1d08d2753413c6f2c90e6f55f65fd51f
3boku <a24746440@gmail.com>
2024-04-24 13:39:41 +0900

    init

--- bmi/app/src/main/java/com/example/bmi_caculator/MainActivity.java
+++ bmi/app/src/main/java/com/example/bmi_caculator/MainActivity.java
@@ -30,37 +30,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         resultButton.setOnClickListener(this);
     }
-    @Override
-    public void onClick(View view) {
-        String str_height = heightEditText.getText().toString();
-        String str_weight = weightEditText.getText().toString();
-
-        if(str_weight.isEmpty() || str_height.isEmpty()){
-            Toast.makeText(this, "빈 값이 있습니다.", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        int height = Integer.parseInt(str_height);
-        int weight = Integer.parseInt(str_weight);
-
-        double bmi = weight / Math.pow(height/100.0, 2.0);
-        resultText.setVisibility(View.VISIBLE);
-        if(bmi >= 35){
-            resultText.setText("고도비만");
-        }
-        else if(bmi >= 30){
-            resultText.setText("중정도비만");
-        }
-        else if(bmi >= 25){
-            resultText.setText("경도비만");
-        }
-        else if(bmi >= 23){
-            resultText.setText("과체중");
-        }
-        else if(bmi >= 18.5){
-            resultText.setText("정상체중");
-        }
-        else if(bmi < 18.5) {
-            resultText.setText("저체중");
-        }
-    }
+    4
 }
