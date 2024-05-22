8378d3d7e98984d9d45e07fc42ab0aa21b021146
3boku <a24746440@gmail.com>
2024-05-22 14:57:03 +0900

    Intent

--- intentTest/app/src/main/java/com/example/intenttest/MainActivity.java
+++ intentTest/app/src/main/java/com/example/intenttest/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.intenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edit = findViewById(R.id.edit);
+
+        findViewById(R.id.btn_next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+
+                intent.putExtra("key", edit.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
--- intentTest/app/src/main/java/com/example/intenttest/SubActivity.java
+++ intentTest/app/src/main/java/com/example/intenttest/SubActivity.java
@@ -2,13 +2,30 @@ package com.example.intenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
 
+    TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        txt = findViewById(R.id.txt);
+
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("key");
+        txt.setText(str);
+
+        findViewById(R.id.btn_prev).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                finish();
+            }
+        });
     }
 }
