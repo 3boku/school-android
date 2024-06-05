18c91eec06d11c4b4750e92e325df2127b40c5be
3boku <a24746440@gmail.com>
2024-06-05 15:07:17 +0900

    study

--- IntentTest0605/app/src/main/java/com/example/intenttest/MainActivity.java
+++ IntentTest0605/app/src/main/java/com/example/intenttest/MainActivity.java
@@ -1,14 +1,78 @@
 package com.example.intenttest;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultTextView = findViewById(R.id.resultTextView);
+
+        findViewById(R.id.dialButton).setOnClickListener(this);
+        findViewById(R.id.mapButton).setOnClickListener(this);
+        findViewById(R.id.smsButton).setOnClickListener(this);
+        findViewById(R.id.next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SubActibity.class);
+
+                launcher.launch(intent);
+            }
+        });
+    }
+
+    ActivityResultLauncher<Intent> launcher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult o) {
+
+                    if(o.getResultCode()==RESULT_OK) {
+                        String str = o.getData().getStringExtra("key");
+                        resultTextView.setText(str);
+                    }
+                }
+            }
+    );
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.dialButton:
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:01012345678"));
+                startActivity(dialIntent);
+
+                break;
+
+            case R.id.mapButton:
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=서울"));
+                startActivity(mapIntent);
+
+                break;
+
+            case R.id.smsButton:
+                Intent smsIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("smsto:01021220739"));
+                smsIntent.putExtra("sms_body", "안녕하세요");
+                startActivity(smsIntent);
+
+                break;
+        }
     }
 }
--- IntentTest0605/app/src/main/java/com/example/intenttest/SubActibity.java
+++ IntentTest0605/app/src/main/java/com/example/intenttest/SubActibity.java
@@ -2,13 +2,31 @@ package com.example.intenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SubActibity extends AppCompatActivity {
+
+    EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        edit = findViewById(R.id.edit);
+        findViewById(R.id.prev).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = getIntent();
+                String str = edit.getText().toString();
+                intent.putExtra("key", str);
+
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 }
