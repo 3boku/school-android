f79d52a3bb5adff4db2119bcc62c799962d07228
3boku <a24746440@gmail.com>
2024-06-19 15:11:21 +0900

    study

--- Shape2/app/src/main/java/com/example/shape/MainActivity.java
+++ Shape2/app/src/main/java/com/example/shape/MainActivity.java
@@ -1,24 +1,79 @@
 package com.example.shape;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.shape.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    ActivityResultLauncher<Intent> launcher;
+
+    int[] selectResult = new int[6];
+    ImageView[] resultImage = new ImageView[2];
+    int[] drawblId = {
+            R.drawable.shape01, R.drawable.shape02, R.drawable.shape03,
+            R.drawable.shape04, R.drawable.shape05, R.drawable.shape06
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(binding.getRoot());
+
+        binding.startImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+
+                if(binding.radioButton1.isChecked()){
+                    intent.putExtra("shape", 0);
+                    launcher.launch(intent);
+                }
+                else if(binding.radioButton2.isChecked()){
+                    intent.putExtra("shape", 1);
+                    launcher.launch(intent);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "찾고 싶은 모양을 클릭하세요", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+
+        resultImage[0] = binding.imageResult01;
+        resultImage[1] = binding.imageResult02;
+
+        launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult o) {
+                        selectResult = o.getData().getIntArrayExtra("result");
+                        int num = 0;
+                        for (int i = 0; i < selectResult.length; i++) {
+                            if(selectResult[i]==1){
+                                resultImage[num].setVisibility(View.VISIBLE);
+                                resultImage[num].setImageResource(drawblId[i]);
+                                num++;
+                            }
+                        }
+
+                    }
+                });
     }
 }
--- Shape2/app/src/main/java/com/example/shape/SubActivity.java
+++ Shape2/app/src/main/java/com/example/shape/SubActivity.java
@@ -1,6 +1,11 @@
 package com.example.shape;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,107 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SubActivity extends AppCompatActivity {
+import com.example.shape.databinding.ActivityMainBinding;
+import com.example.shape.databinding.ActivitySubBinding;
+
+public class SubActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ActivitySubBinding binding;
+
+    private ImageView[] shapeImage = new ImageView[6];
+    private FrameLayout[] shapeFrameLayout = new FrameLayout[6];
+
+    private int[] selectShape = new int[6];
+    private int countShape = 0;
+
+    ImageView checkImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivitySubBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sub);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(binding.getRoot());
+
+        shapeImage[0] = binding.shape01;
+        shapeImage[1] = binding.shape02;
+        shapeImage[2] = binding.shape03;
+        shapeImage[3] = binding.shape04;
+        shapeImage[4] = binding.shape05;
+        shapeImage[5] = binding.shape06;
+
+        shapeFrameLayout[0] = binding.frameLayout01;
+        shapeFrameLayout[1] = binding.frameLayout02;
+        shapeFrameLayout[2] = binding.frameLayout03;
+        shapeFrameLayout[3] = binding.frameLayout04;
+        shapeFrameLayout[4] = binding.frameLayout05;
+        shapeFrameLayout[5] = binding.frameLayout06;
+
+
+        Intent intent = getIntent();
+
+        int shapeNum = intent.getIntExtra("shape", 0);
+
+        if(shapeNum==0){
+            binding.imageView.setImageResource(R.drawable.circle);
+        } else if(shapeNum==1){
+            binding.imageView.setImageResource(R.drawable.square);
+        }
+
+        for (int i = 0; i < shapeImage.length; i++){
+            shapeImage[i].setOnClickListener(this);
+        }
+
+        binding.imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent.putExtra("result", selectShape);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==R.id.shape01){
+            selectUpdate(0);
+        }
+        else if(view.getId()==R.id.shape02){
+            selectUpdate(1);
+        }
+        else if(view.getId()==R.id.shape03){
+            selectUpdate(2);
+        }
+        else if(view.getId()==R.id.shape04){
+            selectUpdate(3);
+        }
+        else if(view.getId()==R.id.shape05){
+            selectUpdate(4);
+        }
+        else if(view.getId()==R.id.shape06){
+            selectUpdate(5);
+        }
+
+    }
+
+    private void selectUpdate(int i) {
+        if(countShape==2 && selectShape[i]==0) {
+            Toast.makeText(this, "선택은 2개까지 가능합니다.", Toast.LENGTH_SHORT).show();
+        }
+        else if (selectShape[i]==1) {
+            selectShape[i]=0;
+            countShape--;
+            shapeFrameLayout[i].removeViewAt(1);
+        }
+        else {
+            selectShape[i] = 1;
+            countShape++;
+
+            checkImage = new ImageView(this);
+            checkImage.setImageResource(R.drawable.ok_check);
+            shapeFrameLayout[i].addView(checkImage);
+        }
+
     }
 }
